32a70600d08bff1b1cbf5b762e5afd82cc9e7b6b
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-06-20 23:07:06 +0700

    fix err not found

--- api/index.js
+++ api/index.js
@@ -1,45 +1,52 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get("/login/", (req,res) =>{ res.render("login.ejs") });
-app.get("/signup/", (req,res) =>{ res.render("signup.ejs") });
-app.get("/signup-seller/", (req,res) =>{ res.render("signup-seller.ejs") });
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs'); // Pastikan view engine diatur ke ejs
+
+app.get("/login/", (req, res) => { res.render("login") });
+app.get("/signup/", (req, res) => { res.render("signup") });
+app.get("/signup-seller/", (req, res) => { res.render("signup-seller") });
 
 // Customer
-app.post("/customer/dashboard/", (req,res) =>{ 
+app.post("/customer/dashboard/", (req, res) => {
     let nama = req.body.email;
-    res.render("customer/index.ejs", { name: nama, });
+    res.render("customer/index", { name: nama });
 });
-app.get("/customer/dashboard/", (req,res) =>{ res.render("customer/index.ejs", { name: "nama", }) });
-app.get("/customer/search/", (req,res) =>{ res.render("customer/search.ejs", { name: "nama", }) });
-app.get("/customer/product/", (req,res) =>{ res.render("customer/product.ejs", { name: "nama", }) });
-app.get("/customer/shop/", (req,res) =>{ res.render("customer/shop.ejs", { name: "nama", }) });
-app.get("/customer/cart/", (req,res) =>{ res.render("customer/cart.ejs", { name: "nama", }) });
-app.get("/customer/tracking/", (req,res) =>{ res.render("customer/tracking.ejs", { name: "nama" }) });
-app.get("/customer/chat/", (req,res) =>{ res.render("customer/chat.ejs", { name: "nama" }) });
+app.get("/customer/dashboard/", (req, res) => { res.render("customer/index", { name: "nama" }) });
+app.get("/customer/search/", (req, res) => { res.render("customer/search", { name: "nama" }) });
+app.get("/customer/product/", (req, res) => { res.render("customer/product", { name: "nama" }) });
+app.get("/customer/shop/", (req, res) => { res.render("customer/shop", { name: "nama" }) });
+app.get("/customer/cart/", (req, res) => { res.render("customer/cart", { name: "nama" }) });
+app.get("/customer/tracking/", (req, res) => { res.render("customer/tracking", { name: "nama" }) });
+app.get("/customer/chat/", (req, res) => { res.render("customer/chat", { name: "nama" }) });
 
 // Seller
-app.get("/seller/dashboard/", (req,res) =>{ res.render("seller/index.ejs", { name: "nama", }) });
-app.get("/seller/shop/", (req,res) =>{ res.render("seller/shop.ejs", { name: "nama", }) });
-app.get("/seller/order/", (req,res) =>{ res.render("seller/order.ejs", { name: "nama", }) });
-app.get("/seller/order-cancel/", (req,res) =>{ res.render("seller/order-cancelled.ejs", { name: "nama", }) });
-app.get("/seller/order-shipping/", (req,res) =>{ res.render("seller/order-shipping.ejs", { name: "nama", }) });
-app.get("/seller/order-review/", (req,res) =>{ res.render("seller/order-review.ejs", { name: "nama", }) });
-app.get("/seller/product/", (req,res) =>{ res.render("seller/product.ejs", { name: "nama", }) });
-app.get("/seller/statistic/", (req,res) =>{ res.render("seller/statistic.ejs", { name: "nama", }) });
+app.get("/seller/dashboard/", (req, res) => { res.render("seller/index", { name: "nama" }) });
+app.get("/seller/shop/", (req, res) => { res.render("seller/shop", { name: "nama" }) });
+app.get("/seller/order/", (req, res) => { res.render("seller/order", { name: "nama" }) });
+app.get("/seller/order-cancel/", (req, res) => { res.render("seller/order-cancelled", { name: "nama" }) });
+app.get("/seller/order-shipping/", (req, res) => { res.render("seller/order-shipping", { name: "nama" }) });
+app.get("/seller/order-review/", (req, res) => { res.render("seller/order-review", { name: "nama" }) });
+app.get("/seller/product/", (req, res) => { res.render("seller/product", { name: "nama" }) });
+app.get("/seller/statistic/", (req, res) => { res.render("seller/statistic", { name: "nama" }) });
 
 // Admin
-app.get("/admin/dashboard/", (req,res) =>{ res.render("admin/index.ejs", { name: "nama", }) });
-app.get("/admin/datatable/", (req,res) =>{ res.render("admin/datatable.ejs", { name: "nama", }) });
-app.get("/admin/statistic-user/", (req,res) =>{ res.render("admin/statistic-user.ejs", { name: "nama", }) });
-app.get("/admin/statistic-seller/", (req,res) =>{ res.render("admin/statistic-seller.ejs", { name: "nama", }) });
-app.get("/admin/statistic-selling/", (req,res) =>{ res.render("admin/statistic-selling.ejs", { name: "nama", }) });
-app.get("/admin/statistic-product/", (req,res) =>{ res.render("admin/statistic-product.ejs", { name: "nama", }) });
-
+app.get("/admin/dashboard/", (req, res) => { res.render("admin/index", { name: "nama" }) });
+app.get("/admin/datatable/", (req, res) => { res.render("admin/datatable", { name: "nama" }) });
+app.get("/admin/statistic-user/", (req, res) => { res.render("admin/statistic-user", { name: "nama" }) });
+app.get("/admin/statistic-seller/", (req, res) => { res.render("admin/statistic-seller", { name: "nama" }) });
+app.get("/admin/statistic-selling/", (req, res) => { res.render("admin/statistic-selling", { name: "nama" }) });
+app.get("/admin/statistic-product/", (req, res) => { res.render("admin/statistic-product", { name: "nama" }) });
 
 export default app;
