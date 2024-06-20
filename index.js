54edbafab74a57f1642ca70fdb5f7a7836d9eb43
unknown <ramata2447@gmail.com>
2024-06-20 16:01:43 +0700

    sign_up_seller

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.static("public"));
 
 app.get("/login/", (req,res) =>{ res.render("login.ejs") });
 app.get("/signup/", (req,res) =>{ res.render("signup.ejs") });
+app.get("/signup-seller/", (req,res) =>{ res.render("signup-seller.ejs") });
 
 // Customer
 app.post("/customer/dashboard/", (req,res) =>{ 
